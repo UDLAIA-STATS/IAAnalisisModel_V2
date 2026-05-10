c90961a8793e9eeb4cd28de082de6987c5535f6e
SebastianG03 <dasedev24@gmail.com>
2026-05-10 00:40:29 -0500

    Models of soccer created for database uses in the future

---
+++ src/entities/models/soccer/ball_model.py
@@ -0,0 +1,19 @@
+from sqlmodel import Field, SQLModel
+
+
+class BallState(SQLModel, table=True):
+    __tablename__ = "ball_states" # type: ignore
+    id: int = Field(primary_key=True, index=True)
+    frame_num: int = Field(index=True)
+    timestamp_ms: float = Field(index=True)
+    conf: float
+    x1: float = Field(nullable=True)
+    y1: float = Field(nullable=True)
+    x2: float = Field(nullable=True)
+    y2: float = Field(nullable=True)
+    dx: float = Field(nullable=True)
+    dy: float = Field(nullable=True)
+    vx: float = Field(nullable=True)
+    vy: float = Field(nullable=True)
+    ax: float = Field(nullable=True)
+    ay: float = Field(nullable=True)
---
+++ src/entities/models/soccer/goal_model.py
@@ -0,0 +1,13 @@
+from sqlmodel import Field, SQLModel
+
+
+class GoalModel(SQLModel, table=True):
+    __tablename__ = "goals" # type: ignore
+    id: int = Field(primary_key=True, index=True)
+    frame_num: int = Field(index=True)
+    timestamp_ms: float = Field(index=True)
+    x1: float = Field(nullable=True)
+    y1: float = Field(nullable=True)
+    x2: float = Field(nullable=True)
+    y2: float = Field(nullable=True)
+    conf: float
