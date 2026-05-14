5d9c6d97bdc2514de4bf36f87370ac0499284871
SebastianG03 <dasedev24@gmail.com>
2026-05-14 16:41:59 -0500

    Added On update column argument

--- src/entities/models/base_models.py
+++ src/entities/models/base_models.py
@@ -14,7 +14,10 @@ class UUIDModel(SQLModel):
 
 class AuditTable(SQLModel):
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
-    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        sa_column_kwargs={"onupdate": datetime.now(timezone.utc)},
+        )
 
 
 class AuditTableCompletedTable(AuditTable, table=True):
