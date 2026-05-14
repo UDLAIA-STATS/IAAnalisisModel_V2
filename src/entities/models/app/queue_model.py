01662543d6dec9059a6027edea6d2772df5596fb
SebastianG03 <dasedev24@gmail.com>
2026-05-14 16:35:18 -0500

    App models relationship fixed

--- src/entities/models/app/queue_model.py
+++ src/entities/models/app/queue_model.py
@@ -1,26 +1,31 @@
-from datetime import datetime, timezone
-from uuid import uuid4
+from typing import List, Optional
 
-from pydantic import BaseModel
-from sqlmodel import Field, Relationship, SQLModel, Column, JSON
-from pydantic import Field as PyField
+from sqlmodel import Field, Relationship
 from entities.models.base_models import (
     AuditTableCompletedTable,
     AuditTable,
+    NumericIdModel,
     UUIDModel)
 from src.entities.types.states import States
 
 
-class AnalysisStep(AuditTableCompletedTable, table=True):
-    id: int
+class AnalysisStep(AuditTableCompletedTable, NumericIdModel, table=True):
+    __tablename__ = "analysisstep" # type: ignore
     name: str = "Analysis Step"
-    state: States = PyField(default=States.PENDING)
+    state: States = Field(default=States.PENDING)
+
+    task_detail_id: str = Field(foreign_key="taskdetail.id", index=True, nullable=False)
+    task_detail: Optional['TaskDetail'] = Relationship(back_populates="steps")
 
 class TaskDetail(UUIDModel, AuditTable, table=True):
+    __tablename__ = "taskdetail" # type: ignore
     task_id: str = Field(foreign_key="task.id", index=True)
-    step: AnalysisStep = Field(sa_column=Column(JSON))
     message: str | None = Field(default=None, nullable=True)
+
     task: 'Task' = Relationship(back_populates="details")
+    steps: List[AnalysisStep] = Relationship(
+        back_populates="task_detail",
+        sa_relationship_kwargs={"cascade": "all, delete-orphan"})
 
 class Task(UUIDModel, AuditTableCompletedTable, table=True):
     match_id: int = Field(index=True)
